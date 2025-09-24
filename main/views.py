cb353def46d6834b2c405ffe423a7ce99dc3a428
michxelle <laudyamichelle0@gmail.com>
2025-09-24 14:40:41 +0700

    Menyelesaikan Tutorial 4

--- main/views.py
+++ main/views.py
@@ -21,7 +21,7 @@ def show_main(request):
 
     context = {
         'npm' : '2406419594',
-        'name': 'Laudya Michelle Alexandra',
+        'name': request.user.username,
         'class': 'PBP A',
         'news_list': news_list,
         'last_login': request.COOKIES.get('last_login', 'Never')
@@ -55,32 +55,6 @@ def show_news(request, id):
 
     return render(request, "news_detail.html", context)
 
-def show_xml(request):
-    news_list = News.objects.all()
-    xml_data = serializers.serialize('xml', news_list)
-    return HttpResponse(xml_data, content_type='application/xml')
-
-def show_json(request):
-    news_list = News.objects.all()
-    json_data = serializers.serialize("json", news_list)
-    return HttpResponse(json_data, content_type="application/json")
-
-def show_xml_by_id(request, news_id):
-   try:
-       news_item = News.objects.filter(pk=news_id)
-       xml_data = serializers.serialize("xml", news_item)
-       return HttpResponse(xml_data, content_type="application/xml")
-   except News.DoesNotExist:
-       return HttpResponse(status=404)
-
-def show_json_by_id(request, news_id):
-   try:
-       news_item = News.objects.get(pk=news_id)
-       json_data = serializers.serialize("json", [news_item])
-       return HttpResponse(json_data, content_type="application/json")
-   except News.DoesNotExist:
-       return HttpResponse(status=404)
-   
 def register(request):
     form = UserCreationForm()
 
@@ -114,3 +88,47 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context) 
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def show_xml(request):
+    news_list = News.objects.all()
+    xml_data = serializers.serialize('xml', news_list)
+    return HttpResponse(xml_data, content_type='application/xml')
+
+def show_json(request):
+    news_list = News.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, news_id):
+   try:
+       news_item = News.objects.filter(pk=news_id)
+       xml_data = serializers.serialize("xml", news_item)
+       return HttpResponse(xml_data, content_type="application/xml")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
+
+def show_json_by_id(request, news_id):
+   try:
+       news_item = News.objects.get(pk=news_id)
+       json_data = serializers.serialize("json", [news_item])
+       return HttpResponse(json_data, content_type="application/json")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
